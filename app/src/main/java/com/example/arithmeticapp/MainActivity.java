bbad360ba688752b2ec880d5dc4a897a9777eb2c
fabin-sunny <fabinsunny@gmail.com>
2024-06-28 09:26:28 +0530

    project created

---
+++ app/src/main/java/com/example/arithmeticapp/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.arithmeticapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    AppCompatButton b1,b2,b3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        b1=(AppCompatButton) findViewById(R.id.largest);
+        b2=(AppCompatButton) findViewById(R.id.smallest);
+        b3=(AppCompatButton) findViewById(R.id.oddeven);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(),LarPage.class);
+                startActivity(i);
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(),SmallPage.class);
+                startActivity(i);
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(),OddPage.class);
+                startActivity(i);
+            }
+        });
+    }
+}
